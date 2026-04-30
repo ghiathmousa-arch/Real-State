e25152fadd714590a83a405afb5b2662959eb6ab
Ghiath-mousa <gieasmousa@gmail.com>
2026-04-30 17:16:57 +0300

    front

--- client/src/api/auth.api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASE = "http://localhost:5000/api";
-
-// تسجيل الدخول
-export const loginApi = async (email: string, password: string) => {
-  const res = await axios.post(`${BASE}/auth/login`, { email, password });
-  return res.data; // يرجع { token, user }
-};
